4ce32fa3f497a841fea3fbf72e1f277d2fb12c07
Atul Vishwakarma <atulvishw240@gmail.com>
2024-10-02 14:52:49 +0530

    Create Game class structure

---
+++ lib/game.rb
@@ -0,0 +1,25 @@
+class Game
+	def initialize(player_1_class, player_2_class)
+		@players = [player_1_class.new(self), player_2_class.new(self)]
+		@board = Array.new(12) { Array.new(4) }
+		@current_player_id = 0
+
+		puts "#{current_player} goes first!!"
+	end
+
+	def current_player_id
+		@current_player_id
+	end
+
+	def current_player
+		@players[current_player_id]
+	end
+
+	def opponent_player_id
+		1 - current_player_id
+	end
+
+	def opponent_player
+		@players[opponent_player_id]
+	end
+end
